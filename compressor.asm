fe2088614ad53fc6de5c4f3fb7324bb8907ff43a
Igor Beduin <igor.rob.96@gmail.com>
2018-05-02 17:56:53 -0300

    Storing words in the stack

--- compressor.asm
+++ compressor.asm
@@ -1,15 +1,16 @@
 .data
     startMessage:   .asciiz     "Hi!\nWelcome to LZW compressor.\nWhat file would you like to compress?\n>> "
     errorMessage:   .asciiz     "\n\nWARNING: Error while trying to open the file!\nClosing the application...\n"
-    successMessage: .asciiz     "\n\nSuccess in opening the file\n!"
+    successMessage: .asciiz     "\n\nSuccess in opening the file!\n"
     fileName:       .space      10 # Reserve 10 bytes to filename be stored.
-    cmpssngMessage: .asciiz     "\n\nCompression started..."
+    cmpssngMessage: .asciiz     "\n\nCompression started...\n\n"
     buffer:         .space      4 # Reserve 4 bytes (1 word) to be read from the file and stored in the buffer
 .text
     main:
         # Initialize registers
         li      $t1, 0
-        li      $s6, 0    # Stores the number of bytes stored in the stack (size)
+        li	$s5, 3
+        li      $s6, 0    # Stores the space reserved in the stack (size)
         li      $s7, 0
 
         # Print the starting message
@@ -28,7 +29,7 @@
         la      $a0, fileName #$a0 has already the adress of 'fileName'
         li      $a1, 0    # Set flag to 'reading' \ 0 for 'reading'; 1 for 'writing'
         li      $a2, 0    # Mode is ignored
-        syscall           # Open the file (and save the descriptor in $v0
+        syscall           # Open the file (and save the descriptor in $v0)
         move    $s7, $v0  # Save the file descriptor in $s7
 
         blt     $s7, 0, op_errorFinishing
@@ -59,8 +60,7 @@
         la      $a0, errorMessage
         syscall
 
-        li      $v0, 10 # Defines syscall to terminate the execution
-        syscall
+	j	Finish
 
     readingLoop:
         li      $v0, 14
@@ -69,11 +69,26 @@
         li      $a2, 4
         syscall
         move    $a3, $a1  # $a3 is the argument to be used inside the other functions with the buffer adress
+        
+        move	$t9, $v0
+        
+        #li	$v0, 4
+        #la	$a0, buffer
+        #syscall
+        
+        move 	$v0, $t9
 
-        li    $t1, -4   # Instruction necessary so in "bne $t1, $s6, searchDict" $t1 has value 0 in the fisrt loop
-        j       searchDict
-
-        #bne    $v0, 0, readingLoop
+        #li    $t1, -4   # Instruction necessary so in "bne $t1, $s6, searchDict" $t1 has value 0 in the fisrt loop
+        #j       searchDict
+        
+        bne     $v0, 0, pushbackDict
+        
+        j Finish
+        
+        
+        li	$v0, 4
+        move	$a1, $a3
+        syscall 
 
         j       Finish
 
@@ -86,11 +101,25 @@
 
         bne     $t1, $s6, searchDict
         li      $t1, 0
+        move	$s5, $zero
         j       pushbackDict
 
     pushbackDict:
-        addi     $sp, $sp, -4
-        sw       $a3, 0($sp)
+    	la	 $a3, buffer
+        addi     $sp, $sp, -8
+        sw       $s5, 0($sp)
+        sw	 $a3, 4($sp)
+        
+        lw	 $t8, 4($sp)
+        li	 $v0, 4
+        move	 $a0, $t8
+        syscall
+        
+        addi 	 $s6, $s6, 4
+        j readingLoop
 
     storeIndex:
         move    $s5, $t0        #$s5 stores the index of the stack (dict) that contains de word that I want
+
+    	
+	
