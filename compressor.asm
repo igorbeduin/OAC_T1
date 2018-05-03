303bb964d838b0346c203f8c6b59f522d96d3ee9
Igor Beduin <igor.rob.96@gmail.com>
2018-05-03 20:46:26 -0300

    Creation of dictionary almost done

--- compressor.asm
+++ compressor.asm
@@ -2,9 +2,11 @@
     startMessage:   .asciiz     "Hi!\nWelcome to LZW compressor.\nWhat file would you like to compress?\n>> "
     errorMessage:   .asciiz     "\n\nWARNING: Error while trying to open the file!\nClosing the application...\n"
     successMessage: .asciiz     "\n\nSuccess in opening the file!\n"
+    readingMessage: .asciiz 	"\n\nReading file...\n" 
     fileName:       .space      10 # Reserve 10 bytes to filename be stored.
     cmpssngMessage: .asciiz     "\n\nCompression started...\n\n"
     buffer:         .space      4 # Reserve 4 bytes (1 word) to be read from the file and stored in the buffer
+    dataArray:	    
 .text
     main:
         # Initialize registers
@@ -18,22 +20,15 @@
         li      $v0, 4    # Defines syscall to print a string
         syscall
 	
-	read_nameFile:
+    read_nameFile:
         # Read the file name string to be read
         li      $v0, 8    # Defines syscall to read a string input
         la      $a0, fileName
         li      $a1, 10   # Defines a max number of characters (bytes) in the input string. Used "dataX.txt" as standard (9 characters + null terminator)
         syscall
-        move $t1, $a0
-#       	trat_quebra_linha:
-#      		lb $t0, ($t1)
-#       		addi $t1, $t1, 1
-#       		bne $t0, '\n', trat_quebra_linha
-#        	addi $t1, $t1, -1
-#        	li $t0, '\0'
-#        	sb $t0, ($t1)      	
+        move 	$t1, $a0      	
 
-	open_file:
+    open_file:
         # Open file
         li      $v0, 13   # Defines syscall to open a file
         la      $a0, fileName #$a0 has already the adress of 'fileName'
@@ -48,19 +43,27 @@
         la      $a0, successMessage
         li      $v0, 4
         syscall
+        
+        # Print that reading file has started
+        la	$a0, readingMessage
+        li	$v0, 4
+        syscall
+     	
+     	j       readFile
 
         # Print that compression has started
         la      $a0, cmpssngMessage
         li      $v0, 4
         syscall
 
-        j       readingLoop
-
     Finish:
         # Close the file
         li      $v0, 16   # Defines syscall to close a file
         move    $a0, $s7  # Sets $a0 to file descriptor
         syscall
+        
+        li	$t1, 0
+        jal 	printDict
 
         li      $v0, 10   # Defines syscall to terminate the execution
         syscall
@@ -72,35 +75,30 @@
 
 	j	Finish
 
-    readingLoop:
+    readFile:
         li      $v0, 14
         move    $a0, $s7
         la      $a1, buffer
         li      $a2, 4
         syscall
-        move    $a3, $a1  # $a3 is the argument to be used inside the other functions with the buffer adress
+        move    $a3, $a1  	# $a3 is the argument to be used inside the other functions with the buffer adress
         
-        move	$t9, $v0
+        move	$t9, $v0	# store the value of $v0 so it can be used in other syscalls
         
         #li	$v0, 4
         #la	$a0, buffer
         #syscall
         
-        move 	$v0, $t9
-
-        #li    $t1, -4   # Instruction necessary so in "bne $t1, $s6, searchDict" $t1 has value 0 in the fisrt loop
-        #j       searchDict
+        move 	$v0, $t9	# restore the value of $v0
+     
+    CallSearcher:   
+        li	$t1, -4
         
-        bne     $v0, 0, pushbackDict
+        bne     $v0, 0, searchDict
         
-        j Finish
+        j 	Finish
         
-        
-        li	$v0, 4
-        move	$a1, $a3
-        syscall 
-
-        j       Finish
+   
 
     searchDict:
         addi    $t1, $t1, 4
@@ -125,11 +123,32 @@
         move	 $a0, $t8
         syscall
         
-        addi 	 $s6, $s6, 4
-        j readingLoop
+        addi 	 $s6, $s6, 8	# Increse the stack size counter register 
+        j readFile
 
     storeIndex:
-        move    $s5, $t0        #$s5 stores the index of the stack (dict) that contains de word that I want
+        move    $s5, $t1      #$s5 stores the index of the stack (dict) that contains de word that I want
+        j 	readFile
+        
+	
+    printDict:
+    	# Print index
+    	lw	$t2, 0($sp)
+    	li	$v0, 1
+    	move	$a0, $t2
+    	syscall
+    	
+    	# Print word
+    	lw	$t2, 4($sp)
+    	li	$v0, 4
+    	move	$a0, $t2
+    	syscall
+    	
+    	addi	$sp, $sp, 8
+    	addi	$t1, $t1, 8
 
+    	bne	$t1, $s6, printDict
+    	
+    	jr 	$ra
     	
 	
