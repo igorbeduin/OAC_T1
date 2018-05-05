f65e127ee16ac062f2efc419152697d24d940017
Igor Beduin <igor.rob.96@gmail.com>
2018-05-05 20:51:29 -0300

    Compression working
    There are some negative indexes found that need to be debugged

--- compressor.asm
+++ compressor.asm
@@ -1,20 +1,20 @@
 .data
-    startMessage:   .asciiz     "Hi!\nWelcome to LZW compressor.\nWhat file would you like to compress?\n>> "
-    errorMessage:   .asciiz     "\n\nWARNING: Error while trying to open the file!\nClosing the application...\n"
-    successMessage: .asciiz     "\n\nSuccess in opening the file!\n"
-    cmpssngFinished: .asciiz 	"\n\nCompression is done!\n" 
-    fileName:       .space      10 # Reserve 10 bytes to filename be stored.
-    cmpssngMessage: .asciiz     "\n\nCompression started...\n\n"
-    buffer:         .space      4 # Reserve 4 bytes (1 word) to be read from the file and stored in the buffer
-    dataArray:	    
+    startMessage:   	.asciiz     	"Hi!\nWelcome to LZW compressor.\nWhat file would you like to compress?\n>> "
+    errorMessage:   	.asciiz     	"\n\nWARNING: Error while trying to open the file!\nClosing the application...\n"
+    successMessage:	.asciiz		"\n\nSuccess in opening the file!\n"
+    cmpssngFinished: 	.asciiz 	"\n\nCompression is done!\n" 
+    fileName:       	.space   	10 		# Reserve 10 bytes to filename be stored.
+    cmpssngMessage: 	.asciiz     	"\n\nCompression started...\n\n"
+    buffer_in:         	.space     	 4 		# Reserve 4 bytes (1 word) to be read from the file and stored in the buffer_in
+    compressedName:    	.space   	10 		# Reserve 10 bytes to filename be stored.
+    writinginFile:	.asciiz 	"\nWriting file: "
 .text
     main:
         # Initialize registers
         li      $t1, 0
-        li	$t9, 0
         li	$s5, 0
         li      $s6, 0    # Stores the space reserved in the stack (size)
-        li      $s7, 0
+        li      $s0, 0
         move	$s4, $sp
 
         # Print the starting message
@@ -31,15 +31,39 @@
         move 	$t1, $a0      	
 
     open_file:
-        # Open file
+        # Open file to be read
         li      $v0, 13   # Defines syscall to open a file
-        la      $a0, fileName #$a0 has already the adress of 'fileName'
+        la      $a0, fileName
         li      $a1, 0    # Set flag to 'reading' \ 0 for 'reading'; 1 for 'writing'
         li      $a2, 0    # Mode is ignored
         syscall           # Open the file (and save the descriptor in $v0)
-        move    $s7, $v0  # Save the file descriptor in $s7
+        move    $s0, $v0  # Save the file descriptor in $s0
 
-        blt     $s7, 0, op_errorFinishing
+        blt     $s0, 0, op_errorFinishing
+        
+        # Change the extension of the fileName from .txt to .lzw
+        li	$t9, 6
+	li	$a1, 108
+	sb	$a1, fileName($t9)
+	
+	li	$t9, 7
+	li	$a1, 122
+	sb	$a1, fileName($t9)
+	
+	li	$t9, 8
+	li	$a1, 119
+	sb	$a1, fileName($t9)
+	
+        # Open file to that will be written
+        li      $v0, 13   # Defines syscall to open a file
+        la      $a0, fileName
+        li      $a1, 1    # Set flag to 'reading' \ 0 for 'reading'; 1 for 'writing'
+        li      $a2, 0    # Mode is ignored
+        syscall           # Open the file (and save the descriptor in $v0)
+        move    $s1, $v0  # Save the file descriptor in $s0
+
+        blt     $s1, 0, op_errorFinishing
+	
 
         # Print success message
         la      $a0, successMessage
@@ -54,12 +78,17 @@
         j       readFile
 
     Finish:
-        # Close the file
+        # Close the reading file
         li      $v0, 16   # Defines syscall to close a file
-        move    $a0, $s7  # Sets $a0 to file descriptor
+        move    $a0, $s0  # Sets $a0 to file descriptor
         syscall
         
-        # Print that compression is started
+        # Close the written file
+        li      $v0, 16   # Defines syscall to close a file
+        move    $a0, $s1  # Sets $a0 to file descriptor
+        syscall
+        
+        # Print that compression has finished
         la      $a0, cmpssngFinished
         li      $v0, 4
         syscall
@@ -79,11 +108,11 @@
 
     readFile:
         li      $v0, 14
-        move    $a0, $s7
-        la      $a1, buffer
+        move    $a0, $s0
+        la      $a1, buffer_in
         li      $a2, 1
         syscall
-        lb   	$a3, 0($a1)		# $a3 is the argument to be used inside the other functions with the buffer adress
+        lb   	$a3, 0($a1)		# $a3 is the argument to be used inside the other functions with the buffer_in adress
 
         seq	$t8, $v0, 0
         beq	$t8, 1, Finish
@@ -116,11 +145,39 @@
         j       pushbackDict
 
     pushbackDict:
-        addi     $sp, $sp, -8
-        sw	 $s5, 0($sp)
-        sw	 $a3, 4($sp)
+        addi    $sp, $sp, -8
+        sw	$s5, 0($sp)
+        sw	$a3, 4($sp)
         
-        li	$t9, 0
+	# Write to written file
+ 	 li   	$v0, 15       # system call for write to file
+ 	 move 	$a0, $s1      # file descriptor 
+ 	 la   	$a1, 0($sp)      # address of buffer_in from which to write 	 
+ 	 li  	$a2, 1	    # hardcoded buffer_in length
+ 	 syscall	        # write to file
+ 	 
+ 	 li  	$v0, 15       # system call for write to file
+ 	 move 	$a0, $s1      # file descriptor 
+ 	 la   	$a1, 4($sp)      # address of buffer_in from which to write 	 
+ 	 li   	$a2, 1	    # hardcoded buffer_in length
+ 	 syscall	
+ 	 
+ 	 # Print that is writing in file
+ 	 li	$v0, 4
+ 	 la	$a0, writinginFile
+ 	 syscall
+ 	 
+ 	 # Print the index that was written in the file
+ 	 li	$v0, 1
+ 	 lb	$a0, 0($sp)
+ 	 syscall
+ 	 
+ 	 # Print the character that was written in the file
+ 	 li	$v0, 11
+ 	 lb	$a0, 4($sp)
+ 	 syscall
+
+        
         move	$s5, $zero
         
         addi 	 $s6, $s6, -8	# Increse the stack size counter register 
@@ -128,7 +185,6 @@
         
     storeIndex:
         sub	$s5, $zero, $t1      #$s5 stores the index of the stack (dict) that contains de word that I want
-        li	$t9, 1
         
         j	readFile
         
